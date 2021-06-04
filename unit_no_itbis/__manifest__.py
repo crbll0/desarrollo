029e43e625a379c778908eaac3e8b0e10794a9f1
crbll0 <caraballo789@gmail.com>
2021-06-04 19:41:00 +0000

    Precio sin itbis

--- unit_no_itbis/__manifest__.py
+++ unit_no_itbis/__manifest__.py
@@ -13,23 +13,17 @@
     'author': "My Company",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'account', 'sale_management'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/sale_order.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/account.xml'
+#         'views/templates.xml',
     ],
 }
